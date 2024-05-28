967fad9f5ea2d32769f03a1101677049d0ee47dd
Miguel Duarte Barroso <mdbarroso@redhat.com>
2024-05-28 17:08:35 +0200

    cni: provide minimal CNI examples
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

---
+++ cmd/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/containernetworking/cni/pkg/types"
+	"runtime"
+
+	"github.com/containernetworking/cni/pkg/skel"
+	current "github.com/containernetworking/cni/pkg/types/100"
+	cniVersion "github.com/containernetworking/cni/pkg/version"
+	"github.com/containernetworking/plugins/pkg/ipam"
+
+	logging "github.com/k8snetworkplumbingwg/cni-log"
+
+	"github.com/maiqueb/devconf2024/pkg/config"
+)
+
+func init() {
+	// this ensures that main runs only on main thread (thread group leader).
+	// since namespace ops (unshare, setns) are done for a single thread, we
+	// must ensure that the goroutine does not jump from OS thread to thread
+	runtime.LockOSThread()
+}
+
+func main() {
+	skel.PluginMainFuncs(
+		skel.CNIFuncs{
+			Add: cmdAdd,
+			Check: func(_ *skel.CmdArgs) error {
+				return nil
+			},
+			Del: func(_ *skel.CmdArgs) error {
+				return nil
+			},
+			GC: func(_ *skel.CmdArgs) error {
+				return nil
+			},
+			Status: status,
+		}, cniVersion.All,
+		"Dummy CNI for learning purposes",
+	)
+}
+
+func status(args *skel.CmdArgs) error {
+	logging.Infof("INVOKED STATUS")
+	netConf, err := loadNetConf(args.StdinData)
+	if err != nil {
+		return fmt.Errorf("failed rendering plugin configuration: %w", err)
+	}
+	logging.Infof("read configuration: %v", netConf)
+
+	if netConf.IPAMConfig != nil {
+		if err := ipam.ExecStatus(netConf.IPAMConfig.Type, args.StdinData); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func cmdAdd(args *skel.CmdArgs) error {
+	logging.Infof("INVOKED ADD")
+	netConf, err := loadNetConf(args.StdinData)
+	if err != nil {
+		return fmt.Errorf("failed rendering plugin configuration: %w", err)
+	}
+	logging.Infof("read configuration: %v", netConf)
+
+	result := &current.Result{
+		CNIVersion: current.ImplementedSpecVersion,
+		Interfaces: []*current.Interface{},
+	}
+
+	if netConf.IPAMConfig != nil {
+		r, err := ipam.ExecAdd(netConf.IPAMConfig.Type, args.StdinData)
+		if err != nil {
+			return err
+		}
+
+		ipamResult, err := current.NewResultFromResult(r)
+		if err != nil {
+			return err
+		}
+
+		result.IPs = ipamResult.IPs
+		result.Routes = ipamResult.Routes
+		result.DNS = ipamResult.DNS
+	}
+
+	return types.PrintResult(result, netConf.CNIVersion)
+}
+
+func loadNetConf(bytes []byte) (*config.NetConf, error) {
+	var conf *config.NetConf
+	if err := json.Unmarshal(bytes, &conf); err != nil {
+		return nil, err
+	}
+	return conf, nil
+}
---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/maiqueb/devconf2024
+
+go 1.21.9
+
+require (
+	github.com/containernetworking/cni v1.2.0
+	github.com/containernetworking/plugins v1.5.0
+	github.com/k8snetworkplumbingwg/cni-log v0.0.0-20230801160229-b6e062c9e0f2
+	github.com/lib/pq v1.10.9
+)
+
+require (
+	github.com/coreos/go-iptables v0.7.0 // indirect
+	github.com/safchain/ethtool v0.3.0 // indirect
+	github.com/vishvananda/netlink v1.2.1-beta.2 // indirect
+	github.com/vishvananda/netns v0.0.4 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
